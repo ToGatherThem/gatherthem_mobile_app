fc9c27ea59ec3995b25dc0278e5e69430113c85f
DjibrilQS <d.quenumsanfo@gmail.com>
2022-04-14 09:19:43 +0200

    [UPD] collectionsItems post sql change

--- lib/models/collection_model.dart
+++ lib/models/collection_model.dart
@@ -1,14 +1,18 @@
+import 'package:gatherthem_mobile_app/models/collection_item_model.dart';
+
 class CollectionModel{
   String id;
   String name;
   String description;
   String creationDate;
+  List<CollectionItemModel> items;
 
   CollectionModel.fromJson(Map json):
     id = json['id'],
     name = json['name'],
     description = json['description'],
-    creationDate = json['creationDate'];
+    creationDate = json['creationDate'],
+    items = json['properties']['id'];
 
 
 }
--- lib/ui/screens/collection_screen.dart
+++ lib/ui/screens/collection_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:gatherthem_mobile_app/globals.dart';
+import 'package:gatherthem_mobile_app/models/collection_item_model.dart';
 import 'package:gatherthem_mobile_app/models/collection_model.dart';
 import 'package:gatherthem_mobile_app/theme/strings.dart';
 import 'package:intl/intl.dart';
+import '../tiles/collection_tile.dart';
 import '../widgets/navigation_scaffold_widget.dart';
 
 class CollectionScreen extends StatelessWidget {
@@ -18,71 +21,99 @@ class CollectionScreen extends StatelessWidget {
     TextStyle titleStyle = const TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
     TextStyle subtitleStyle = const TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
     TextStyle descriptionStyle = const TextStyle(fontSize: 16);
-    return SizedBox(
+    return Scaffold(
+        body:Column(
+          children: [
+            SizedBox(
       width : MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height/3,
       child: Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-        child: Container(
-          color: Theme.of(context).backgroundColor,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
+            padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+            child: Container(
+              color: Theme.of(context).backgroundColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
-                    child: Icon(
-                      Icons.add_photo_alternate_rounded,
-                      color: Theme.of(context).primaryColor,
-                      size: 80,
-                    ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                            collection.name,
-                            style: titleStyle
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Icon(
+                          Icons.add_photo_alternate_outlined,
+                          color: Theme.of(context).primaryColor,
+                          size: 80,
                         ),
-                        // TODO need to be add with template
-                        /*Text(
-                            collection.type,
-                            style: subtitleStyle
-                        ),*/
-                        Text(
-                            DateFormat(Strings.dayFormat).format(DateTime.parse(collection.creationDate)).toString(),
-                            style: subtitleStyle
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                collection.name,
+                                style: titleStyle
+                            ),
+                            // TODO need to be add with template
+                            /*Text(
+                                collection.type,
+                                style: subtitleStyle
+                            ),*/
+                            Text(
+                                DateFormat(Strings.dayFormat).format(DateTime.parse(collection.creationDate)).toString(),
+                                style: subtitleStyle
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              const Padding(padding: EdgeInsets.only(top: 10)),
-              Expanded(
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width/1.2,
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Card(
-                      elevation: 0,
-                      margin: EdgeInsets.zero,
+                  const Padding(padding: EdgeInsets.only(top: 10)),
+                  Expanded(
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width/1.2,
                       child: Padding(
-                        padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
-                        child: Text(
-                          collection.description,
-                          style: descriptionStyle,
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Card(
+                          elevation: 0,
+                          margin: EdgeInsets.zero,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+                            child: Text(
+                              collection.description,
+                              style: descriptionStyle,
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
       ),
+    ),
+            StreamBuilder<List<CollectionModel>>(
+              stream: blocCollection.stream,
+              builder: (context, snapshot) {
+                return Padding(
+                        padding: const EdgeInsets.only(left: 15, right: 15),
+                        child: Card(
+                          elevation: 0,
+                          child: Padding(
+                            padding:
+                            const EdgeInsets.only(top: 20, bottom: 20),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(collection.items.first.toString()),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+
+              }
+            ) ],
+          )
+
     );
   }
 }
