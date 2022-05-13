a9394407103d16497882beb1eb0e928c3886486e
Vincent Mignot <vinc.mignot@hotmail.fr>
2022-05-13 11:45:19 +0200

    [UPD] updating collection tile UI/UX

--- lib/ui/tiles/collection_tile.dart
+++ lib/ui/tiles/collection_tile.dart
@@ -6,6 +6,7 @@ import 'package:gatherthem_mobile_app/globals.dart';
 import 'package:gatherthem_mobile_app/models/collection_model.dart';
 import 'package:gatherthem_mobile_app/services/collection_service.dart';
 import 'package:gatherthem_mobile_app/ui/screens/collection_screen.dart';
+import 'package:gatherthem_mobile_app/ui/screens/edit_collection_screen.dart';
 
 
 class CollectionTile extends StatelessWidget {
@@ -21,93 +22,75 @@ class CollectionTile extends StatelessWidget {
       stream: isEdition.stream,
       initialData: false,
       builder: (context, snapshot) {
-        return Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Theme.of(context).cardColor,
+        if (snapshot.data ?? false) {
+          return Stack(
+            children: [
+              ImageFiltered(
+                imageFilter: ImageFilter.blur(
+                  sigmaX: 1,
+                  sigmaY: 1,
+                  tileMode: TileMode.decal
                 ),
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) =>
-                        CollectionScreen(
-                            collection: collection
-                        )
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Theme.of(context).cardColor,
+                    ),
+                    child: Padding(
+                      padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.photo,
+                            size: 55,
+                          ),
+                          const Padding(padding: EdgeInsets.only(left: 20)),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  collection.template?.fullName ?? '',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16, color: Theme
+                                      .of(context)
+                                      .primaryColor
+                                  ),
+                                ),
+                                Text(
+                                  collection.name,
+                                  style: TextStyle(
+                                      fontSize: 16, color: Theme
+                                      .of(context)
+                                      .primaryColor
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Visibility(
+                            visible: snapshot.data != null && !snapshot.data!,
+                            child: IconButton(
+                                onPressed: () => isEdition.setBool(snapshot.data != null && !snapshot.data!),
+                                icon: Icon(
+                                  Icons.more_vert_rounded,
+                                  color: Theme.of(context).primaryColor,
+                                )
+                            ),
+                          )
+                        ],
                       ),
-                    );
-                  },
-                  child: Padding(
-                    padding:
-                    const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(
-                          Icons.photo,
-                          size: 55,
-                        ),
-                        const Padding(padding: EdgeInsets.only(left: 20)),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                collection.template?.fullName ?? '',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16, color: Theme
-                                    .of(context)
-                                    .primaryColor
-                                ),
-                              ),
-                              Text(
-                                collection.name,
-                                style: TextStyle(
-                                    fontSize: 16, color: Theme
-                                    .of(context)
-                                    .primaryColor
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Visibility(
-                          visible: snapshot.data != null && !snapshot.data!,
-                          child: IconButton(
-                              onPressed: () => isEdition.setBool(snapshot.data != null && !snapshot.data!),
-                              icon: Icon(
-                                Icons.more_vert_rounded,
-                                color: Theme.of(context).primaryColor,
-                              )
-                          ),
-                        )
-                      ],
                     ),
                   ),
                 ),
               ),
-            ),
-            Visibility(
-              visible: snapshot.data ?? false,
-              child: BackdropFilter(
-                filter: ImageFilter.blur(
-                    sigmaX: 2,
-                    sigmaY: 2,
-                ),
-                child: Container(
-                  color: Colors.transparent,
-                ),
-              ),
-            ),
-            Visibility(
-              visible: snapshot.data ?? false,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -115,13 +98,22 @@ class CollectionTile extends StatelessWidget {
                     Column(
                       children: [
                         FloatingActionButton(
-                            onPressed: () {},
-                            mini: true,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50)
-                            ),
+                          heroTag: 'edit ${collection.id}',
+                          onPressed: () async{
+                            Navigator.push(context, MaterialPageRoute(
+                                builder: (context) =>  EditCollectionScreen(
+                                    collection: collection)));
+                          },
+                          mini: true,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)
+                          ),
+                          backgroundColor: Colors.orange,
+                          child: const Icon(Icons.edit_outlined),
                         ),
+                        const Padding(padding: EdgeInsets.only(top: 5)),
                         FloatingActionButton(
+                            heroTag: 'delete ${collection.id}',
                             onPressed: () async {
                               bool res = await CollectionService().deleteCollection(collection.id);
                               if(res){
@@ -130,7 +122,7 @@ class CollectionTile extends StatelessWidget {
                             },
                             mini: true,
                             backgroundColor: Colors.red,
-                            child: Icon(Icons.delete)
+                            child: const Icon(Icons.delete_outline_rounded)
                         ),
                       ],
                     ),
@@ -144,9 +136,81 @@ class CollectionTile extends StatelessWidget {
                   ],
                 ),
               ),
+            ],
+          );
+        }
+        else{
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Theme.of(context).cardColor,
+              ),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) =>
+                        CollectionScreen(
+                            collection: collection
+                        )
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding:
+                  const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.photo,
+                        size: 55,
+                      ),
+                      const Padding(padding: EdgeInsets.only(left: 20)),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              collection.template?.fullName ?? '',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16, color: Theme
+                                  .of(context)
+                                  .primaryColor
+                              ),
+                            ),
+                            Text(
+                              collection.name,
+                              style: TextStyle(
+                                  fontSize: 16, color: Theme
+                                  .of(context)
+                                  .primaryColor
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Visibility(
+                        visible: snapshot.data != null && !snapshot.data!,
+                        child: IconButton(
+                            onPressed: () => isEdition.setBool(snapshot.data != null && !snapshot.data!),
+                            icon: Icon(
+                              Icons.more_vert_rounded,
+                              color: Theme.of(context).primaryColor,
+                            )
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ],
-        );
+          );
+        }
+
       }
     );
   }
