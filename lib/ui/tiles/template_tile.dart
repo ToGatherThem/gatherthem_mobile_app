dc8916422e4a129fc9ee7748853a8df742c84563
Vincent Mignot <vinc.mignot@hotmail.fr>
2022-05-13 17:16:16 +0200

    [UPD] updating logic behind add collection from the template list

--- lib/ui/tiles/template_tile.dart
+++ lib/ui/tiles/template_tile.dart
@@ -1,55 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:gatherthem_mobile_app/theme/styles.dart';
+import 'package:gatherthem_mobile_app/ui/screens/add_collection_screen.dart';
 
 class TemplateTile extends StatelessWidget{
 
   final String templateName;
   final IconData templateIcon;
+  final String? templateFullName;
 
 
-  const TemplateTile({Key? key, required this.templateName, required this.templateIcon}) : super(key: key);
+  const TemplateTile({Key? key, required this.templateName, required this.templateIcon, this.templateFullName}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).cardColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            children: [
-              Icon(
-                  templateIcon,
-                  size: 50,
-                  color: Theme.of(context).backgroundColor,
-              ),
-              Positioned(
-                top: 25,
-                left: 30,
-                child: Container(
-                  width: 20,
-                  height: 20,
-                  decoration: BoxDecoration(
+    return InkWell(
+      onTap: (){
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddCollectionScreen(templateFullName: templateFullName,))
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Icon(
+                    templateIcon,
+                    size: 50,
                     color: Theme.of(context).backgroundColor,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Icon(
-                      Icons.add,
-                    size: 15,
-                    color: Colors.white,
-                  ),
                 ),
-              )
-            ],
-          ),
-          Text(
-              'Ajouter des $templateName',
-            style: Styles.getTextStyle(context, color: Theme.of(context).backgroundColor),
-          ),
-        ],
+                Positioned(
+                  top: 25,
+                  left: 30,
+                  child: Container(
+                    width: 20,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).backgroundColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Icon(
+                        Icons.add,
+                      size: 15,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Text(
+                'Ajouter des $templateName',
+              style: Styles.getTextStyle(context, color: Theme.of(context).backgroundColor),
+            ),
+          ],
+        ),
       ),
     );
   }
