69aed6a4b5fbc638a121a29f2c4081a5102cdf40
Bryan Prolong <smyllet.gamer@gmail.com>
2022-06-02 11:26:03 +0200

    [UPD] Add image in item model

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,12 +1,17 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 class ItemModel {
   String id;
   String label;
   String creationDate;
   String obtentionDate;
+  Uint8List? image;
 
   ItemModel.fromJson(Map json):
     id = json["id"],
     label = json["label"],
     creationDate = json["creationDate"],
-    obtentionDate = json["obtentionDate"];
+    obtentionDate = json["obtentionDate"],
+    image = (json["image"] != null) ? const Base64Decoder().convert(json["image"]).buffer.asUint8List() : null;
 }
