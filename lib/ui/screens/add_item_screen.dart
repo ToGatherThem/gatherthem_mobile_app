659066a5a306d5eb7cca7121b9eb21e1124af7dd
Yanis Briffaut <yanis.briffaut@gmail.com>
2022-06-24 15:12:41 +0200

    Hihi i forgot screens

--- lib/ui/screens/add_item_screen.dart
+++ lib/ui/screens/add_item_screen.dart
@@ -56,6 +56,7 @@ class _AddItemScreenState extends State<AddItemScreen> {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     InkWell(
+                      borderRadius: BorderRadius.circular(10),
                       onTap: () => selectImageModal.show(
                           context: context,
                           onImageSelected: (image) {
