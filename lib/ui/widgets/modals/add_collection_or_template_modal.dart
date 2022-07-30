171aaef576ed0c1c887bc6edcfdc5a1bb2e54061
Vincent Mignot <vinc.mignot@hotmail.fr>
2022-07-30 14:59:08 +0200

    [FIX] remove unused import

--- lib/ui/widgets/modals/add_collection_or_template_modal.dart
+++ lib/ui/widgets/modals/add_collection_or_template_modal.dart
@@ -1,12 +1,7 @@
-import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:gatherthem_mobile_app/theme/strings.dart';
 import 'package:gatherthem_mobile_app/ui/screens/add_collection_screen.dart';
 import 'package:gatherthem_mobile_app/ui/screens/add_template_screen.dart';
-import 'package:image_cropper/image_cropper.dart';
-import 'package:image_picker/image_picker.dart';
 
 class AddCollectionOrTemplateModal {
   static void show({required BuildContext context
